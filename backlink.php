0a9e256504112a9383ad78cb3bff96071ee1474e
beatkarbu199 <153447374+beatkarbu199@users.noreply.github.com>
2024-02-01 18:16:15 +0700

    Create backlink.php

---
+++ backlink.php
@@ -0,0 +1,16 @@
+   <div style="display: none;">
+<a href="https://gokups.menlhk.go.id/uploads/konten/ibu/">bandar togel</a>
+<a href="https://social.unib.ac.id/wp-content/ibu-gcorr/">slot gacor</a>
+<a href="http://ppkh.menlhk.go.id/pgone/">slot gacor</a>
+<a href="https://social.unib.ac.id/wp-content/ibu-gcorr/">slot online</a>
+<a href="https://inspirationmars.com/">slot gacor</a>
+<a href="https://e-sakip.situbondokab.go.id/js/ibu/">bandar togel</a>
+<a href="https://lamongankab.go.id/beranda/xigacorr/index.php">slot gacor</a>
+<a href="https://education.unib.ac.id/wp-content/pasti-gcorr/">slot gacor</a>
+<a href="https://lamongankab.go.id/beranda/xigacorr/index.php">slot gacor</a>
+<a href="https://social.unib.ac.id/wp-content/ibu-gcorr/">slot gacor</a>
+<a href="https://e-sakip.situbondokab.go.id/jgcor/"">slot gacor</a>
+<a href="https://ansit-stunting.probolinggokota.go.id/xigcorr/">slot gacor</a>
+<a href="https://e-meso.pom.go.id/adrreport2/laboratorium/bdr/">bandar togel</a>
+<a href="https://e-meso.pom.go.id/bpom/ibu/">slot gacor</a>
+     </div>
