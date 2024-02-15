fa49cb9924a052cf33af98a44cc3dcff3d217fc3
beatkarbu199 <153447374+beatkarbu199@users.noreply.github.com>
2024-02-15 21:15:48 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -8,7 +8,7 @@
 <a href="https://lamongankab.go.id/beranda/xigacorr/index.php">slot gacor</a>
 <a href="https://vhrmedia.com/">slot gacor</a>
 <a href="https://e-sakip.situbondokab.go.id/img/sigcor/">slot gacor</a>
-<a href="https://ulcc.unitomo.ac.id/assets/img/ibu/">IBU4D</a>
+<a href="https://dealwithculture.com/">IBU4D</a>
 <a href="https://e-sakip.situbondokab.go.id/jgcor/"">slot gacor</a>
 <a href="https://www.gantengidaman.pro/">ibu4d</a>
 <a href="https://fisip.umj.ac.id/wp-content/server/">slot thailand</a>
@@ -18,10 +18,10 @@
 <a href="https://www.dealwithculture.com/">ibu4d</a>
 <a href="https://jaki.pta-bandung.go.id/images/bdrtgl/">bandar togel terpercaya</a>
 <a href="https://sipenmaru.poltekkes-smg.ac.id/storage/cmsz/">slot gacor</a>
-<a href="https://ciruas.serangkab.go.id/storage/ibu/">slot gacor</a>
+<a href="https://banjarnegarakab.go.id/main/wp-content/gacor-bos/">slot gacor</a>
 <a href="https://jaki.pta-bandung.go.id/images/bdrtgl/api/lp.php">slot gacor</a>
 <a href="https://banjarnegarakab.go.id/main.html">slot gacor</a>
-<a href="https://ciruas.serangkab.go.id/storage/ibu/">slot online</a>
+<a href="https://ciruas.serangkab.go.id/storage/ibu/">slot gacor</a>
 <a href="https://ab.mtsnkotamadiun.sch.id/sw-content/4d/index.php">IBU4D</a>
 <a href="https://disnaker.indramayukab.go.id/wp.html">slot gacor</a>
 <a href="https://data-sektoral.pringsewukab.go.id/uploads/tgl/">bandar togel</a>
