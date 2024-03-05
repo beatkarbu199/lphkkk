1bb7ea952bb6cb307deb3c89c61808d7e2579cbb
beatkarbu199 <153447374+beatkarbu199@users.noreply.github.com>
2024-03-05 16:46:30 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -20,7 +20,7 @@
 <a href="https://sdi.babelprov.go.id/sektoral/pasti-gacorr/">slot 4d</a>
 <a href="https://purbasari.com/ibu/">slot 4d</a>
 <a href="https://jaki.pta-bandung.go.id/images/bdrtgl/api/lp.php">slot gacor</a>
-<a href="https://espt.mitrakab.go.id/filesurat/kamboja/">slot 888</a>
+<a href="https://petaling.belitung.go.id/js/css/">slot gacor</a>
 <a href="https://lppmp.unimed.ac.id/wp-content/img/">slot gacor</a>
 <a href="https://isfm.faperika.unri.ac.id/wp.html">slot gacor</a>
 <a href="https://e-renggar.kemkes.go.id/images/img/thai/">slot gacor</a>
